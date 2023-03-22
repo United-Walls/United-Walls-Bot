4c8cf0ef56ab61a0d8052f9c15ca6493f2ffcae3
paraskcd1315 <paraskhanchandani1315@gmail.com>
2023-03-21 21:16:54 -0400

    Changes

--- api/walls.js
+++ api/walls.js
@@ -4,6 +4,56 @@ const router = express.Router();
 const TgBot = require('../TgBot')
 
 /*
+Route -		GET api/walls/count
+Desc -		Get Walls Count
+Access - 	Public
+*/
+router.get('/count', async (req, res) => {
+	try {
+		const count = await Walls.count();
+		return res.json(count)
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({
+			errors: [
+				{
+					msg: 'WTF did you do now? Fuck you! This is a fucking Server Error, thanks for fucking it up asshole!',
+				},
+			],
+		});
+	} 
+})
+
+/*
+Route -     GET api/walls
+Desc -      Get all walls
+Access -    Public
+*/
+
+router.get('/queries', async (req, res) => {
+	try {
+		const page = req.query.page;
+		const numberOfWalls = 50;
+
+		const walls = await Walls.find().skip(page * numberOfWalls).limit(numberOfWalls).sort({ createdAt: -1 }).collation({
+			locale: 'en_US',
+			numericOrdering: true,
+		});
+
+		return res.json(walls);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({
+			errors: [
+				{
+					msg: 'WTF did you do now? Fuck you! This is a fucking Server Error, thanks for fucking it up asshole!',
+				},
+			],
+		});
+	}
+});
+
+/*
 Route -     GET api/walls/update
 Desc -      Get all walls
 Access -    Public
