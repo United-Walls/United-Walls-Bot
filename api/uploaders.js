a88ecd242002cdfb3e177f42ebe0ce62c4fe7634
paraskcd1315 <paraskhanchandani1315@gmail.com>
2023-07-13 20:34:11 -0400

    Uploader APIs

--- api/uploaders.js
+++ api/uploaders.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Uploader = require('../models/Uploader');
-const Walls = require('../models/Walls');
 
 /*
 Route -     GET api/uploaders/
@@ -11,11 +10,11 @@ Access -    Public
 
 router.get("/", async (req, res) => {
     try {
-        const uploaders = Uploader.find().populate({ path: 'walls' });
+        const uploaders = await Uploader.find();
 
         return res.json(uploaders);
     } catch(err) {
-        console.error(err.message);
+        console.error(err);
 		TgBot.api.sendMessage(
 			-1001731686694,
 			`Error: Hey, @ParasKCD, wake up! There was an error in the United Walls Server. Might have crashed, don't know.\n\nHere's the Error\n\n${err.message}`, { message_thread_id: 77299 }
