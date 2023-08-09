2c3e74cb2285ee95c14f727e33d086fea428edc0
paraskcd1315 <paraskhanchandani1315@gmail.com>
2023-08-09 19:47:28 -0400

    Fix admin bugs

--- api/creators/wallpapers.js
+++ api/creators/wallpapers.js
@@ -8,6 +8,38 @@ const router = express.Router();
 const fs = require('fs');
 
 /*
+Route -     POST api/creators/wallpapers/admin?wallId=:wallId
+Desc -      Update a wallpaper
+Access -    Public
+*/
+router.post('/admin', verifyToken, async (req, res) => {
+    try {
+        const { wallId } = req.query;
+        const { file_name, category } = req.body;
+        const cat = await Category.findById(category);
+        const wall = await Walls.findById(wallId);
+
+        if (req.user.userID === 975024565
+            || req.user.userID === 934949695
+            || req.user.userID === 1889905927 
+            || req.user.userID === 127070302  
+            ) {
+            const updatedWall = await Walls.findByIdAndUpdate(wallId, { file_name, category: cat });
+            return res.status(201).json(updatedWall);
+        } else {
+            return res.status(401).send("Ok who the fuck? Bro are you lost? If you got this message, it means you either fucked up something, don't have the necessary stuff I need, aren't authorized, or you just a plain dumbass. Now Fuck OFF!");
+        }
+    } catch (err) {
+        console.error(err);
+        TgBot.api.sendMessage(
+			-1001731686694,
+			`Error: Hey, @ParasKCD, wake up! There was an error in the United Walls Server. Might have crashed, don't know.\n\nHere's the Error\n\n${err.message}`, { message_thread_id: 77299 }
+		);
+        return res.status(500).send("Something went wrong. Please try again.");
+    }
+});
+
+/*
 Route -     POST api/creators/wallpapers?wallId=:wallId
 Desc -      Update a wallpaper
 Access -    Public
@@ -44,42 +76,112 @@ Access -    Public
 router.delete('/', verifyToken, async (req, res) => {
     try {
         const { wallId } = req.query;
-        const deletedWall = await Walls.findByIdAndDelete(wallId);
-        console.log("deletedwall", deletedWall);
-        let category = await Category.findByIdAndUpdate(deletedWall.category, {
-            $pull: { walls: deletedWall.id }
-        });   
-        await Uploader.findByIdAndUpdate(deletedWall.creator, {
-            $pull: { walls: deletedWall.id }
-        });
-        category = await Category.findById(deletedWall.category);
-        if (category.walls.length == 0) {
-            await Category.findByIdAndDelete(deletedWall.category);
+        const wall = await Walls.findById(wallId);
+        const creator = await Uploader.findById(wall.creator);
+
+        if (req.user.id === creator.id) {
+            const deletedWall = await Walls.findByIdAndDelete(wallId);
+            console.log("deletedwall", deletedWall);
+            let category = await Category.findByIdAndUpdate(deletedWall.category, {
+                $pull: { walls: deletedWall.id }
+            });   
+            await Uploader.findByIdAndUpdate(deletedWall.creator, {
+                $pull: { walls: deletedWall.id }
+            });
+            category = await Category.findById(deletedWall.category);
+            if (category.walls.length == 0) {
+                await Category.findByIdAndDelete(deletedWall.category);
+            }
+
+            fs.stat(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext}`, function (err, stats) {
+                if (err) {
+                    return console.error(err);
+                }
+            
+                fs.unlink(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext}`,function(err){
+                    if(err) return console.log(err);
+                    console.log(`${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext} file deleted successfully from temp folder`);
+                });  
+            });
+
+            fs.stat(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/thumbnails/${deletedWall.file_name}.${deletedWall.file_ext}`, function (err, stats) {
+                if (err) {
+                    return console.error(err);
+                }
+            
+                fs.unlink(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/thumbnails/${deletedWall.file_name}.${deletedWall.file_ext}`,function(err){
+                    if(err) return console.log(err);
+                    console.log(`${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext} file deleted successfully from temp folder`);
+                });  
+            });
+
+            return res.status(200).json(deletedWall);
+        } else {
+            return res.status(401).send("Ok who the fuck? Bro are you lost? If you got this message, it means you either fucked up something, don't have the necessary stuff I need, aren't authorized, or you just a plain dumbass. Now Fuck OFF!");
         }
+    } catch (err) {
+        console.error(err);
+        TgBot.api.sendMessage(
+			-1001731686694,
+			`Error: Hey, @ParasKCD, wake up! There was an error in the United Walls Server. Might have crashed, don't know.\n\nHere's the Error\n\n${err.message}`, { message_thread_id: 77299 }
+		);
+        return res.status(500).send("Something went wrong. Please try again.");
+    }
+});
 
-        fs.stat(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext}`, function (err, stats) {
-            if (err) {
-                return console.error(err);
+/*
+Route -     DELETE api/creators/wallpapers/admin?wallId=:wallId
+Desc -      Delete a wallpaper
+Access -    Public
+*/
+router.delete('/admin', verifyToken, async (req, res) => {
+    try {
+        const { wallId } = req.query;
+        if (
+            req.user.userID === 975024565
+            || req.user.userID === 934949695
+            || req.user.userID === 1889905927 
+            || req.user.userID === 127070302
+        ) {
+            const deletedWall = await Walls.findByIdAndDelete(wallId);
+            console.log("deletedwall", deletedWall);
+            let category = await Category.findByIdAndUpdate(deletedWall.category, {
+                $pull: { walls: deletedWall.id }
+            });   
+            await Uploader.findByIdAndUpdate(deletedWall.creator, {
+                $pull: { walls: deletedWall.id }
+            });
+            category = await Category.findById(deletedWall.category);
+            if (category.walls.length == 0) {
+                await Category.findByIdAndDelete(deletedWall.category);
             }
-         
-            fs.unlink(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext}`,function(err){
-                 if(err) return console.log(err);
-                 console.log(`${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext} file deleted successfully from temp folder`);
-            });  
-        });
 
-        fs.stat(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/thumbnails/${deletedWall.file_name}.${deletedWall.file_ext}`, function (err, stats) {
-            if (err) {
-                return console.error(err);
-            }
-         
-            fs.unlink(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/thumbnails/${deletedWall.file_name}.${deletedWall.file_ext}`,function(err){
-                 if(err) return console.log(err);
-                 console.log(`${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext} file deleted successfully from temp folder`);
-            });  
-        });
+            fs.stat(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext}`, function (err, stats) {
+                if (err) {
+                    return console.error(err);
+                }
+            
+                fs.unlink(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext}`,function(err){
+                    if(err) return console.log(err);
+                    console.log(`${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext} file deleted successfully from temp folder`);
+                });  
+            });
 
-        return res.status(200).json(deletedWall);
+            fs.stat(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/thumbnails/${deletedWall.file_name}.${deletedWall.file_ext}`, function (err, stats) {
+                if (err) {
+                    return console.error(err);
+                }
+            
+                fs.unlink(`./storage/wallpapers/${category.name.replace(/\s/g, '').trim()}/thumbnails/${deletedWall.file_name}.${deletedWall.file_ext}`,function(err){
+                    if(err) return console.log(err);
+                    console.log(`${category.name.replace(/\s/g, '').trim()}/${deletedWall.file_name}.${deletedWall.file_ext} file deleted successfully from temp folder`);
+                });  
+            });
+
+            return res.status(200).json(deletedWall);
+        } else {
+            return res.status(401).send("Ok who the fuck? Bro are you lost? If you got this message, it means you either fucked up something, don't have the necessary stuff I need, aren't authorized, or you just a plain dumbass. Now Fuck OFF!");
+        }
     } catch (err) {
         console.error(err);
         TgBot.api.sendMessage(
